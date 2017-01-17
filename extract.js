904da18df9f5d8193a6a6595504b2f54944be3f6
Yue Shi <yueshi277@gmail.com>
2017-01-16 21:25:08 -0500

    reject err

--- extract.js
+++ extract.js
@@ -7,30 +7,35 @@ const url = 'http://web-aaronding.rhcloud.com/employee.html';
 function getData(url) {
 
 	return new Promise(function (resolve, reject) {
-		request(url, function (error, response, html) {
+		request(url, function (err, res, html) {
 			var employee = [];
+			var obj = {};
+			const keys = ['firstName', 'lastName', 'ext', 'cell', 'alt', 'title', 'email'];
 
-			if (!error) {
-				const $ = cheerio.load(html);
-				const keys = ['firstName', 'lastName', 'ext', 'cell', 'alt', 'title', 'email'];
-				var obj = {};
-
-				$('tr:not(:first-child)').filter(function (i, el) {
-					var data = $(el).find('td');
-
-					if (data.length) {
-						data.each(function (i, el) {
-							obj[keys[i]] = $(el).text();
-						});
-
-						employee.push(Object.assign({}, obj));
-					}
-				});
-				resolve(employee);
-
-			} else {
-				return reject(error);
+			if (err) {
+				return reject(err);
+			} else if (res.statusCode !== 200) {
+				err = new Error("Unexpected status code: " + res.statusCode);
+				err.res = res;
+				return reject(err);
 			}
+
+			const $ = cheerio.load(html);
+
+			$('tr:not(:first-child)').filter(function (i, el) {
+				var data = $(el).find('td');
+
+				if (data.length) {
+					data.each(function (i, el) {
+						obj[keys[i]] = $(el).text();
+					});
+
+					employee.push(Object.assign({}, obj));
+				}
+			});
+			
+			resolve(employee);
+
 		});
 	});
 }
