b06eabcccd2c989aed5f1b07ac063e860630553a
Yue Shi <yueshi277@gmail.com>
2017-01-18 10:30:24 -0500

    adjust format

--- extract.js
+++ extract.js
@@ -4,31 +4,31 @@ var rp = require('request-promise');
 const url = 'http://web-aaronding.rhcloud.com/employee.html';
 
 var options = {
-	uri: url,
-	transform: function (body) {
-		return cheerio.load(body);
-	}
+  uri: url,
+  transform: function (body) {
+	return cheerio.load(body);
+  }
 };
 
 rp(options)
-	.then(function ($) {
-		var employee = [];
-		var obj = {};
-		const keys = ['firstName', 'lastName', 'ext', 'cell', 'alt', 'title', 'email'];
+  .then(function ($) {
+	var obj = {};
+	var employee = [];
+	const keys = ['firstName', 'lastName', 'ext', 'cell', 'alt', 'title', 'email'];
 
-		$('tr:not(:first-child)').filter(function (i, el) {
-			var data = $(el).find('td');
+	$('tr:not(:first-child)').filter(function (i, el) {
+	  var data = $(el).find('td');
 
-			if (data.length) {
-				data.each(function (i, el) {
-					obj[keys[i]] = $(el).text();
-				});
-
-				employee.push(Object.assign({}, obj));
-			}
+	  if (data.length) {
+	  	data.each(function (i, el) {
+		  obj[keys[i]] = $(el).text();
 		});
-		console.log(employee);
-	})
-	.catch(function (err) {
-		console.error("%s; %s", err.error.message, err.options.url);
+
+		employee.push(Object.assign({}, obj));
+      }
 	});
+	console.log(employee);
+  })
+  .catch(function (err) {
+  	console.error("%s; %s", err.error.message, err.options.url);
+  });
